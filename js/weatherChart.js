4993f0c8d7f484d114ba05cf650222ea3fdacfb5
github username <github email address>
2018-02-09 14:25:33 -0500

    Done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,35 @@
+var API_KEY = "692ae25469c02f8c";
+var CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
+var URL = CORS_WRAPPER + "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
+
 function getFahrenheits(result){
-  // Your code goes here
+  return result.hourly_forecast.map(function(data) {
+    return data.temp.english
+  })
 }
+  // Your code goes here
+
 
 function getHours(result){
+  return result.hourly_forecast.map(function(data) {
+    return data.FCTTIME.hour
+  })
   // Your code goes here
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  let returnObj = { labels: labels,
+    datasets: [{
+           label: "My First dataset",
+           backgroundColor: 'rgb(255, 99, 132)',
+           data: data,
+         }]
+       }
+  return returnObj
 }
+  // Your code goes here
 
 function makeRequest(endpoint, success) {
   // Your code goes here
-}
+  return fetch(endpoint).then(res => res.json()).then(json => success(json))
+  }
